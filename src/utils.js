b7d1be413c605fee831e31658f70702ae70e3a90
Sebastian Sastre <sebastianconcept@gmail.com>
2019-09-07 20:44:22 -0300

    fixes unpublish and published date

--- src/utils.js
+++ src/utils.js
@@ -8,3 +8,10 @@ export function post (endpoint, data) {
     }
   }).then(r => r.json())
 }
+
+// https://stackoverflow.com/a/13052187/485081
+Date.prototype.toDateInputValue = function () {
+  var local = new Date(this)
+  local.setMinutes(this.getMinutes() - this.getTimezoneOffset())
+  return local.toJSON().slice(0, 10)
+}
