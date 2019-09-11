219dad2d6a57398c916bc9e69170e37ed1a0e99f
Sebastian Sastre <sebastianconcept@gmail.com>
2019-09-11 08:17:13 -0300

    domain change

--- src/config/environment.js
+++ src/config/environment.js
@@ -7,7 +7,8 @@ module.exports = {
 function getEnvironment () {
   return {
     production: {
-      baseUrl: `https://blog.sebastiansastre.co`
+      // baseUrl: `https://blog.sebastiansastre.co`
+      baseUrl: `https://sebastiansastre.co`
     },
     development: {
       baseUrl: `http://localhost:3000`
